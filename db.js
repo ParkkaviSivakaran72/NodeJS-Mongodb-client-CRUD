defd432af1e0a019466c726ee17aa08e4f74ba07
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-01-13 10:06:05 +0530

    practise with node js and mongodb client

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongodb=require('mongodb')
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId
+let database;
+
+async function getDatabase(){
+    const client = await MongoClient.connect('mongodb://127.0.0.1:27017'),
+    database = client.db('office');
+    if(!database){
+        console.log('database is not connected')
+    }
+    return database;
+}
+
+module.exports = {
+    getDatabase,
+    ObjectId
+}
